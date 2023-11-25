85fe9a3949d85212c8e4eb09ed4645dc1ea42303
Alexander Royal <alexander.de.royal@gmail.com>
2023-11-26 03:45:54 +0545

    starter files created

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from "./navbar.module.css";
+
+export const Navbar = () => {
+    return (
+        <div className={styles.container}>Navbar</div>
+    )
+}
+
