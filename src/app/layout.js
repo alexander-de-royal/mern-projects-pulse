92392ecf4c720e78eea4831d35cc20e2bc991aa3
Alexander Royal <alexander.de.royal@gmail.com>
2023-11-26 11:00:49 +0545

    40:12

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,32 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import {Navbar} from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+import {ThemeContextProvider} from "@/context/ThemeContext";
+import {ThemeProvider} from "@/providers/ThemeProvider";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
+  title: 'Pulse',
+  description: 'Your health in your pocket.',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <div className="container">
-          <div className="wrapper">
-          {children}
-          </div>
-      </div>
+        <ThemeContextProvider>
+            <ThemeProvider>
+                <div className="container">
+                    <div className="wrapper">
+                        <Navbar/>
+                        {children}
+                        <Footer/>
+                    </div>
+                </div>
+            </ThemeProvider>
+        </ThemeContextProvider>
       </body>
     </html>
   )
