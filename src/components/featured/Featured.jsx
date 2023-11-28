b7c9d27059d4a9bdd1dea10a16ea003057348b41
Alexander Royal <alexander.de.royal@gmail.com>
2023-11-28 09:22:19 +0545

    category component design completed:01:05:55

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 export const Featured = () => {
     return (
-        <div className={styles.container}>Featured</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                <b>Hey, lama dev here!</b> Discover my stories and creative ideas.
+            </h1>
+            <div className={styles.post}>
+                <div className={styles.imgContainer}>
+                    <Image src="/p1.jpeg" alt="" width={550} height={500}/>
+                </div>
+                <div className={styles.textContainer}>
+                    <h1>Lorem ipsum dolor sit amet alim consectetur adipiscing elt</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus expedita molestiae non porro quibusdam ullam. Consequuntur minima nihil odio velit!
+                    </p>
+                    <button className={styles.button}>Read More</button>
+                </div>
+            </div>
+        </div>
     )
 }
