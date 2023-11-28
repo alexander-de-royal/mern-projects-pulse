53ea8c0c1f78813db3ad0249842fe9d5013e1f3c
Alexander Royal <alexander.de.royal@gmail.com>
2023-11-28 16:52:26 +0545

    1:15:04

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import styles from "./cardList.module.css";
+import {Pagination} from "@/components/pagination/Pagination";
+import {Card} from "@/components/card/Card";
 
 export const CardList = () => {
     return (
-        <div className={styles.container}>CardList</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Recent Posts</h1>
+            <div className={styles.posts}>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+            <Pagination/>
+        </div>
     )
 }
