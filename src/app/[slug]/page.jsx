2bc0885438e937e60e7aa82548b641ade7b2114c
Alexander Royal <alexander.de.royal@gmail.com>
2023-12-09 17:39:31 +0545

    1:52:14

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styles from "./singlePage.module.css";
+import {Menu} from "@/components/Menu/Menu";
+import Image from "next/image";
+
+const SinglePage = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.infoContainer}>
+                <div className={styles.textContainer}>
+                    <h1 className={styles.title}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+                    <div className={styles.user}>
+                        <div className={styles.userImageContainer}>
+                            <Image src="/p1.jpeg" alt="" fill className={styles.avatar}/>
+                        </div>
+                        <div className={styles.userTextContainer}>
+                            <span className={styles.username}>John Doe</span>
+                            <span className={styles.date}>01.01.2024</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+                </div>
+            </div>
+            <div className={styles.content}>
+                <div className={styles.post}>
+                    <div className={styles.description}>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab animi, aspernatur commodi consequatur cumque cupiditate doloremque dolorum eaque, facilis incidunt inventore ipsa magni nihil officia pariatur provident quia quis quo quos recusandae reprehenderit saepe sapiente similique unde veritatis vero!
+                        </p>
+                        <h5>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab animi, aspernatur commodi consequatur cumque cupiditate doloremque dolorum eaque, facilis incidunt inventore ipsa magni nihil officia pariatur provident quia quis quo quos recusandae reprehenderit saepe sapiente similique unde veritatis vero!
+                        </h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab animi, aspernatur commodi consequatur cumque cupiditate doloremque dolorum eaque, facilis incidunt inventore ipsa magni nihil officia pariatur provident quia quis quo quos recusandae reprehenderit saepe sapiente similique unde veritatis vero!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab animi, aspernatur commodi consequatur cumque cupiditate doloremque dolorum eaque, facilis incidunt inventore ipsa magni nihil officia pariatur provident quia quis quo quos recusandae reprehenderit saepe sapiente similique unde veritatis vero!
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default SinglePage;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from "./blogPage.module.css";
 import {CardList} from "@/components/cardList/CardList";
 import {Menu} from "@/components/Menu/Menu";
+import {Card} from "@/components/card/Card";
+import {CategoryList} from "@/components/categoryList/CategoryList";
 
 const BlogPage = () => {
     return (
@@ -9,8 +11,10 @@ const BlogPage = () => {
             <h1 className={styles.title}>Style Blog</h1>
             <div className={styles.content}></div>
             <div>
+                <CategoryList/>
                 <CardList/>
-                <Menu/>
+                <Card/>
+                {/*<Menu/>*/}
             </div>
         </div>
     )
