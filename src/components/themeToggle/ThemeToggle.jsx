f35b85a655ff4849541472514690ecb2229df93d
Alexander Royal <alexander.de.royal@gmail.com>
2023-12-12 19:10:45 +0545

    session work but new errors showed up

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -8,7 +8,7 @@ import {ThemeContext} from "@/context/ThemeContext";
 export const ThemeToggle = () => {
 
     const {theme, toggle} = useContext(ThemeContext)
-    console.log(theme)
+  
 
     return (
         <div className={styles.container} onClick={toggle}>
